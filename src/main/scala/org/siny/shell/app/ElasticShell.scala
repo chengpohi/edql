ce00e579d3beddd3299272a98e7a3adafb557c4a
chengpohi <chengpohi@gmail.com>
2015-06-10 23:13:59 +0800

    DONE:initial project --- TODO:

---
+++ src/main/scala/org/siny/shell/app/ElasticShell.scala
@@ -0,0 +1,11 @@
+package org.siny.shell.app
+
+/**
+ * Elastic Shell
+ * Created by chengpohi on 6/10/15.
+ */
+object ElasticShell {
+  def main(args: Array[String]): Unit = {
+    println("Hello")
+  }
+}
