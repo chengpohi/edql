98d6c1830e1ca2dd99f81d517f96cd4d4e2c822a
chengpohi <chengpohi@gmail.com>
2016-01-24 18:30:43 +0800

    DONE:analysis api---TODO:

--- src/main/scala/com/github/chengpohi/parser/ELKInstrumentParser.scala
+++ src/main/scala/com/github/chengpohi/parser/ELKInstrumentParser.scala
@@ -17,24 +17,24 @@ class ELKInstrumentParser {
   val variable: P[String] = P(strName.rep.!).map(s => "$" + s)
   val parameter: P[String] = P(space ~ strParameter ~ ",".? ~ space)
 
-  val status: P[(String, Option[Seq[String] => String], Seq[String])] = P("health").map(s => ("health", Some(ELKCommand.h), Seq()))
-  val count: P[(String, Option[Seq[String] => String], Seq[String])] = P("count" ~ space ~ (strParameter | variable)).map(c =>
+  val status = P("health").map(s => ("health", Some(ELKCommand.h), Seq()))
+  val count = P("count" ~ space ~ (strParameter | variable)).map(c =>
     ("count", Some(ELKCommand.c), Seq(c)))
-  val delete: P[(String, Option[Seq[String] => String], Seq[String])] = P("delete" ~ space ~ (strParameter | variable)).map(c =>
+  val delete = P("delete" ~ space ~ (strParameter | variable)).map(c =>
     ("delete", Some(ELKCommand.d), Seq(c)))
-  val query: P[(String, Option[Seq[String] => String], Seq[String])] = P("query" ~ space ~ (strParameter | variable)).map(c =>
+  val query = P("query" ~ space ~ (strParameter | variable)).map(c =>
     ("query", Some(ELKCommand.q), Seq(c)))
-  val reindex: P[(String, Option[Seq[String] => String], Seq[String])] = P("reindex" ~ space ~ (strParameter | variable).rep(4, sep = " ")).map(
+  val reindex = P("reindex" ~ space ~ (strParameter | variable).rep(4, sep = " ")).map(
     c => ("reindex", Some(ELKCommand.r), c))
-  val index: P[(String, Option[Seq[String] => String], Seq[String])] = P("index" ~ space ~ (strParameter | variable).rep(3, sep = " ")).map(
+  val index = P("index" ~ space ~ (strParameter | variable).rep(3, sep = " ")).map(
     c => ("index", Some(ELKCommand.i), c)
   )
-  val createIndex: P[(String, Option[Seq[String] => String], Seq[String])] = P("createIndex" ~ space ~ (strParameter | variable)).map(
+  val createIndex = P("createIndex" ~ space ~ (strParameter | variable)).map(
     c => ("createIndex", Some(ELKCommand.ci), Seq(c))
   )
-
-  val update: P[(String, Option[Seq[String] => String], Seq[String])] = P("update"
-    ~ space ~ (strParameter | variable).rep(3, sep = " ")).map(c =>
+  val update = P("update" ~ space ~ (strParameter | variable).rep(3, sep = " ")).map(c =>
     ("query", Some(ELKCommand.u), c))
+  val analysis = P("analysis" ~ space ~ (strParameter | variable).rep(2, sep=" ")).map(c =>
+    ("analysis", Some(ELKCommand.a), c))
   val functionInstrument = P(strName.rep.! ~ "(" ~/ parameter.rep ~ ")").map(f => (f._1, None, f._2))
 }
