47ad732d659a56751d959c2556fc77f7ba0254e4
Xiang Chen <chengpohi@gmail.com>
2016-02-06 14:40:55 +0800

    DONE:construct collection with string, int, tuple and array---TODO:
    Former-commit-id: 7c9ffb35ccc125cd28e146ca7cb11b15d1d7ee5b

--- src/main/scala/com/github/chengpohi/parser/CollectionParser.scala
+++ src/main/scala/com/github/chengpohi/parser/CollectionParser.scala
@@ -9,7 +9,7 @@ class CollectionParser {
   import fastparse.all._
 
   val StringChars = NamedFunction(!"\"\\".contains(_: Char), "StringChars")
-  val CollectionChars = NamedFunction(!",()\"\\".contains(_: Char), "CollectionChars")
+  val CollectionChars = NamedFunction(!"[],()\"\\".contains(_: Char), "CollectionChars")
   val space = P(CharsWhile(" \r\n".contains(_)).?)
 
   val strChars = P(CharsWhile(StringChars))
@@ -19,10 +19,11 @@ class CollectionParser {
   val variable: P[String] = P(variableChars.rep(1)).!.map(s => "$" + s)
   val parameter: P[String] = P(space ~ strParameter ~ ",".? ~ space)
   val strOrVar: P[String] = P(strParameter | variable)
+  val number: P[Int] = P(CharIn('0' to '9').rep(1)).!.map(_.toInt)
 
-  val tuple: P[Seq[String]] = P("(" ~ collectionChars.!.rep(1, sep = ",".~/) ~ ")")
-  val array: P[Seq[Seq[String]]] = P("[" ~ tuple.rep(1, sep = ",".~/) ~ "]")
-  val collection = P(space ~ (tuple | array) ~ space)
+  val tuple: P[Seq[Any]] = P("(" ~ collection.rep(1, sep = space ~ "," ~ space.~/) ~ ")")
+  val array: P[Seq[Any]] = P("[" ~ collection.rep(1, sep = space ~ "," ~ space.~/) ~ "]")
+  val collection = P(space ~ (tuple | array | number | strOrVar) ~ space)
 }
 
 case class NamedFunction[T, V](f: T => V, name: String) extends (T => V) {
--- src/test/scala/com/github/chengpohi/parser/CollectionParserTest.scala
+++ src/test/scala/com/github/chengpohi/parser/CollectionParserTest.scala
@@ -15,12 +15,16 @@ class CollectionParserTest extends FlatSpec with BeforeAndAfter {
   import collectionParser._
 
   "collection" should "parse tuple" in {
-    val Parsed.Success(value, _) = collection.parse("(1,2,3)")
-    assert(value === ArrayBuffer("1", "2", "3"))
+    val Parsed.Success(value1, _) = collection.parse("(1,2,3)")
+    assert(value1 === ArrayBuffer(1, 2, 3))
   }
 
   "collection" should "parse array" in {
     val Parsed.Success(value, _) = collection.parse("[(1,2,3),(4,5,6)]")
-    assert(value === ArrayBuffer(ArrayBuffer("1", "2", "3"), ArrayBuffer("4", "5", "6")))
+    val Parsed.Success(value2, _) = collection.parse("[(1, 2, 3), 7, 6]")
+    val Parsed.Success(value3, _) = collection.parse("""[("1", "2", 3), 7, 6]""")
+    assert(value === ArrayBuffer(ArrayBuffer(1,2, 3), ArrayBuffer(4, 5, 6)))
+    assert(value2 === ArrayBuffer(ArrayBuffer(1, 2, 3), 7, 6))
+    assert(value3 === ArrayBuffer(ArrayBuffer("1", "2", 3), 7, 6))
   }
 }
