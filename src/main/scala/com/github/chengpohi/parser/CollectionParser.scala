27a0e7a7abb669b13aafaa54b87359f115156b71
chengpohi <chengpohi@gmail.com>
2016-02-19 22:25:00 +0800

    DONE:apply json structure---TODO:
    Former-commit-id: 7094f8565f39c319c27ff7dcff148db25037ef2b

--- src/main/scala/com/github/chengpohi/parser/CollectionParser.scala
+++ src/main/scala/com/github/chengpohi/parser/CollectionParser.scala
@@ -1,5 +1,7 @@
 package com.github.chengpohi.parser
 
+import com.github.chengpohi.collection.Js
+
 /**
  * elasticshell
  * Created by chengpohi on 2/1/16.
@@ -15,22 +17,31 @@ class CollectionParser {
   val strChars = P(CharsWhile(StringChars))
   val collectionChars = P(CharsWhile(CollectionChars))
   val variableChars = P(CharIn('a' to 'z', 'A' to 'Z'))
-  val string: P[String] = P("\"" ~ strChars.rep(1).! ~ "\"")
-  val variable: P[String] = P(variableChars.rep(1)).!.map(s => "$" + s)
-  val parameter: P[String] = P(space ~ string ~ ",".? ~ space)
-  val strOrVar: P[String] = P(string | variable)
-  val number: P[Int] = P(CharIn('0' to '9').rep(1)).!.map(_.toInt)
-  val pair = P(space ~/ string ~/ space ~/ ":" ~/ collection)
-  val obj: P[Seq[Any]] = P("{" ~ space ~/ pair.rep(sep = ",".~/) ~ space ~ "}")
+  val string = P("\"" ~ strChars.rep(1).! ~ "\"").map(Js.Str)
+  val variable = P(variableChars.rep(1)).!.map(s => "$" + s).map(Js.Str)
+  //val parameter: P[String] = P(space ~ string ~ ",".? ~ space)
+  val strOrVar = P(string | variable)
+  val number = P(CharIn('0' to '9').rep(1)).!.map(x => Js.Num(x.toDouble))
+  val pair = P(space ~/ string.map(_.value) ~/ space ~/ ":" ~/ jsonExpr)
+  val obj = P("{" ~ space ~/ pair.rep(sep = ",".~/) ~ space ~ "}").map(Js.Obj(_: _*))
+
+  val `null` = P("null").map(_ => Js.Null)
+  val `false` = P("false").map(_ => Js.False)
+  val `true` = P("true").map(_ => Js.True)
 
 
-  val tuple: P[Seq[Any]] = P("(" ~ collection.rep(1, sep = space ~ "," ~ space.~/) ~ ")")
-  val array: P[Seq[Any]] = P("[" ~ collection.rep(1, sep = space ~ "," ~ space.~/) ~ "]")
-  val collection = P(space ~ (obj | tuple | array | number | strOrVar) ~ space)
-  val ioParser = P(collection.rep(1, sep = space))
+  val tuple = P("(" ~ jsonExpr.rep(1, sep = space ~ "," ~ space.~/) ~ ")").map(Js.Arr(_: _*))
+  val array = P("[" ~ jsonExpr.rep(1, sep = space ~ "," ~ space.~/) ~ "]").map(Js.Arr(_: _*))
+  //val collection = P(space ~ (obj | tuple | array | number | strOrVar) ~ space)
+
+  val jsonExpr: P[Js.Val] = P(
+    space ~ (obj | array | tuple | string | `true` | `false` | `null` | number) ~ space
+  )
+  val ioParser: P[Seq[Js.Val]] = P(jsonExpr.rep(1, sep = space))
 }
 
 case class NamedFunction[T, V](f: T => V, name: String) extends (T => V) {
   def apply(t: T) = f(t)
+
   override def toString() = name
 }
