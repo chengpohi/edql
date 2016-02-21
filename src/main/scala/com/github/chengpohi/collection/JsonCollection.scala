e2a00c4c6659b3e9e21e4af1ecdb76c80e60debe
Xiang Chen <chengpohi@gmail.com>
2016-02-21 23:40:00 +0800

    toJson
    Former-commit-id: c3b4705b01c8d89450838b991315557598f5a0db

--- src/main/scala/com/github/chengpohi/collection/JsonCollection.scala
+++ src/main/scala/com/github/chengpohi/collection/JsonCollection.scala
@@ -1,6 +1,5 @@
 package com.github.chengpohi.collection
 
-import scala.reflect.api.JavaUniverse
 import scala.reflect.runtime.universe._
 
 /**
@@ -15,28 +14,48 @@ object JsonCollection {
     def apply(i: Int): Val = this.asInstanceOf[Arr].value(i)
 
     def apply(s: java.lang.String): Val = this.asInstanceOf[Obj].value.find(_._1 == s).get._2
+
+    def toJson: String
   }
 
-  case class Str(value: java.lang.String) extends AnyVal with Val
+  case class Str(value: java.lang.String) extends AnyVal with Val {
+    override def toJson: String = "\"" + value + "\""
+  }
 
-  case class Obj(value: (java.lang.String, Val)*) extends AnyVal with Val
+  case class Obj(value: (java.lang.String, Val)*) extends AnyVal with Val {
+    override def toJson: String = value.map {
+      case (n, v) => "{\"" +  n + "\":" + v.toJson + "}"
+    }.mkString(",")
+  }
 
-  case class Arr(value: Val*) extends AnyVal with Val
+  case class Arr(value: Val*) extends AnyVal with Val {
+    override def toJson: String = "[" + value.map(i => i.toJson).mkString(",") + "]"
+  }
 
-  case class Tuple(value: Val*) extends AnyVal with Val
+  case class Tuple(value: Val*) extends AnyVal with Val {
+    override def toJson: String = "(" + value.map(i => i.toJson).mkString(",") + ")"
+  }
 
-  case class Num(value: Double) extends AnyVal with Val
+  case class Num(value: Double) extends AnyVal with Val {
+    override def toJson: String = value.toString
+  }
 
   case object False extends Val {
     def value = false
+
+    override def toJson: String = value.toString
   }
 
   case object True extends Val {
     def value = true
+
+    override def toJson: String = value.toString
   }
 
   case object Null extends Val {
     def value = null
+
+    override def toJson: String = value.toString
   }
 
   implicit def numToDouble(n: Num): java.lang.Double = {
@@ -47,7 +66,7 @@ object JsonCollection {
     v.value.asInstanceOf[T]
   }
 
-  def choiceType(tp: Type) = {
+  def chooseType(tp: Type) = {
     if (tp =:= typeOf[Int]) {
       typeTag[Int]
     } else {
@@ -58,7 +77,7 @@ object JsonCollection {
   def extract[T](v: Val)(implicit tag: TypeTag[T]): T = {
     if (tag.tpe <:< typeOf[List[_]]) {
       val subType: Type = tag.tpe.typeArgs.head
-      v.asInstanceOf[Arr].value.toList.map(i => extract(i)(choiceType(subType))).asInstanceOf[T]
+      v.asInstanceOf[Arr].value.toList.map(i => extract(i)(chooseType(subType))).asInstanceOf[T]
     } else if (tag.tpe =:= typeOf[Int]) {
       v.asInstanceOf[Num].value.toInt.asInstanceOf[T]
     } else {
