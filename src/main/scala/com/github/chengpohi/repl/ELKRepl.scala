1ed3be115a8b84744e3099883bb5a08030022c1a
Xiang Chen <chengpohi@gmail.com>
2016-03-24 21:35:51 +0800

    DONE:auto completion for repl---TODO: help usage
    Former-commit-id: cb7a9b96df6bc069c7b4cdc13f7cb9abd4f2dcd3
    Former-commit-id: 7e20d09091b68ee043e83693e7e76aede8228bd4

--- src/main/scala/com/github/chengpohi/repl/ELKRepl.scala
+++ src/main/scala/com/github/chengpohi/repl/ELKRepl.scala
@@ -3,17 +3,25 @@ package com.github.chengpohi.repl
 import java.io.PrintWriter
 
 import com.github.chengpohi.ELKRunEngine
+
+import scala.collection.JavaConversions._
 import jline.console.ConsoleReader
+import jline.console.completer.StringsCompleter
+
+import scala.io.Source
 
 /**
  * elasticshell
  * Created by chengpohi on 1/27/16.
  */
 object ELKRepl {
+  val terms = new StringsCompleter(Source.fromURL(getClass.getResource("/completions.txt")).getLines().toSeq.distinct)
+
   def main(args: Array[String]): Unit = {
     val reader = new ConsoleReader()
     reader.setPrompt("elasticshell>")
-    val out = new PrintWriter(reader.getOutput())
+    reader.addCompleter(terms)
+    val out = new PrintWriter(reader.getOutput)
     while (true) {
       val line = reader.readLine()
       if (line == "exit") System.exit(0)
