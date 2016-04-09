c474999dfbf597a651608bb43a45a734d7d60a95
chengpohi <chengpohi@gmail.com>
2016-04-09 23:13:05 +0800

    DONE:create analyzer for test---TODO:
    Former-commit-id: c85aa395b943040b8d2dfbd7dd417421d8cf2066
    Former-commit-id: 9802ccb30ee0f3870e7c44679267097947d83e4c

--- src/main/scala/com/github/chengpohi/repl/ELKRepl.scala
+++ src/main/scala/com/github/chengpohi/repl/ELKRepl.scala
@@ -9,24 +9,38 @@ import jline.console.ConsoleReader
 import scala.io.Source
 
 /**
- * elasticshell
- * Created by chengpohi on 1/27/16.
- */
+  * elasticshell
+  * Created by chengpohi on 1/27/16.
+  */
 object ELKRepl {
+  val ELASTIC_SHELL_INDEX_NAME: String = ".elasticshell"
   val terms = new StringsCompleter(Source.fromURL(getClass.getResource("/completions.txt")).getLines().toSet)
   val eLKCompletionHandler = new ELKCompletionHandler
   val elkRunEngine = new ELKRunEngine(ELKCommandRegistry)
 
+
   def main(args: Array[String]): Unit = {
+    beforeStart()
+
     val reader = new ConsoleReader()
     reader.setPrompt("elasticshell>")
     reader.addCompleter(terms)
     reader.setCompletionHandler(eLKCompletionHandler)
+
     val out = new PrintWriter(reader.getOutput)
     while (true) {
       val line = reader.readLine()
       if (line == "exit") System.exit(0)
       elkRunEngine.run(line)
     }
+  }
+
+  def beforeStart() = {
+    elkRunEngine.run(s"""create index "$ELASTIC_SHELL_INDEX_NAME"""")
+    Runtime.getRuntime.addShutdownHook(new Thread {
+      override def run(): Unit = {
+        elkRunEngine.run(s"""delete "$ELASTIC_SHELL_INDEX_NAME"""")
+      }
+    })
   }
 }
