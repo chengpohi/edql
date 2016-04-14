cdd54101e9f766d502a260ae40f3d378b7ed62f0
Xiang Chen <chengpohi@gmail.com>
2016-04-14 23:20:10 +0800

    DONE:enable cluster name configure^M---TODO:
    Former-commit-id: 0ae46c18317010a6224340d14393b2b9e7c11adc
    Former-commit-id: 46de5355fc7f045da9f1d1d8b6567a9f5e88b999

--- src/main/scala/com/github/chengpohi/connector/ElasticClientConnector.scala
+++ src/main/scala/com/github/chengpohi/connector/ElasticClientConnector.scala
@@ -1,21 +1,26 @@
 package com.github.chengpohi.connector
 
-import com.sksamuel.elastic4s.{ElasticsearchClientUri, ElasticClient}
+import com.sksamuel.elastic4s.{ElasticClient, ElasticsearchClientUri}
 import com.typesafe.config.ConfigFactory
+import org.elasticsearch.common.settings.Settings
 
 /**
-*  elasticshell
-*  Created by chengpohi on 3/19/15.
-*/
+  * elasticshell
+  * Created by chengpohi on 3/19/15.
+  */
 object ElasticClientConnector {
   lazy val indexConfig = ConfigFactory.load("application.conf").getConfig("elastic")
+  val clusterName: String = indexConfig.getString("cluster.name")
+  val settings = Settings.settingsBuilder()
+    .put("cluster.name", clusterName)
+    .build()
   val host: String = indexConfig.getString("host")
   val port: Int = indexConfig.getInt("port")
 
-  val client = buildClient(host, port)
+  val client = buildClient(settings, host, port)
 
-  def buildClient(host: String, port: Int) = {
+  def buildClient(settings: Settings, host: String, port: Int) = {
     val uri = ElasticsearchClientUri(s"elasticsearch://$host:$port")
-    ElasticClient.transport(uri)
+    ElasticClient.transport(settings, uri)
   }
 }
