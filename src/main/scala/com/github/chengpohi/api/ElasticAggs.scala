076e50f76b83c3903325b5d02f7ff8ca9e1b3af4
chengpohi <chengpohi@gmail.com>
2016-07-16 01:23:37 +0800

    DONE:aggs term query
    Former-commit-id: a9bb01f5a212391eafc904ee4a01e07c3a3fb892
    Former-commit-id: 1ef8a015390c927784e0dc347b69fdd6def21d31

--- src/main/scala/com/github/chengpohi/api/ElasticAggs.scala
+++ src/main/scala/com/github/chengpohi/api/ElasticAggs.scala
@@ -5,14 +5,18 @@ import org.elasticsearch.action.search.SearchResponse
 
 import scala.concurrent.Future
 
-
 /**
   * elasticshell
   * Created by chengpohi on 3/12/16.
   */
-trait ElasticAggs extends AggsDSL{
+trait ElasticAggs extends AggsDSL {
+
   import DSLHelper._
+
   def aggsSearch(indexName: String, indexType: String, name: String): Future[SearchResponse] = ElasticExecutor {
     aggs in indexName / indexType avg name
+  }
+  def termsSearch(indexName: String, indexType: String, name: String): Future[SearchResponse] = ElasticExecutor {
+    aggs in indexName / indexType term name
   }
 }
