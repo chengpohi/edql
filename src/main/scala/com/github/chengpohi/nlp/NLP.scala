10d98a4bde6faef0eefe075db427ad1bf30aeafa
chengpohi <chengpohi@gmail.com>
2016-08-24 18:29:43 +0800

    DONE:
    Former-commit-id: 6dd21f12e58b8c7fd1483aa1a01c6fb6eb5ec0d0
    Former-commit-id: 2cfceca6d2a1e9be5b1a374f34d6f0f6dfd554ad

--- src/main/scala/com/github/chengpohi/nlp/NLP.scala
+++ src/main/scala/com/github/chengpohi/nlp/NLP.scala
@@ -10,13 +10,11 @@ object NLP {
   implicit class Str(a: String) {
     def distance(b: String): Int = a.foldLeft((0 to b.length).toList)((prev, x) =>
       (prev zip prev.tail zip b).scanLeft(prev.head + 1) {
-        case (h, ((d, v), y)) => {
-          min(min(h + 1, v + 1), d + (if (x == y) 0 else 1))
-        }
+        case (h, ((d, v), y)) => min(min(h + 1, v + 1), d + (if (x == y) 0 else 1))
       }) last
   }
 
-  implicit class It[K](it: Seq[K]) {
+  implicit class Words[K](it: Seq[K]) {
     def unigram = ngram(1)
     def bigram = ngram(2)
     def ngram(n: Int): Stream[Seq[K]] = it.sliding(n).toStream
