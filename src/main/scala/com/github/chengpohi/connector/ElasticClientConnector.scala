efe9ab512b9009e24f30a84dc00fb4df16d1774f
Xiang Chen <chengpohi@gmail.com>
2016-09-20 23:41:23 +0800

    DONE:local elasticshell
    Former-commit-id: ae08150a9da29aaefcdeb2a18b420286263f3c1d
    Former-commit-id: fdb3fd610caac76c811b23b530867ec09e8d650b

--- src/main/scala/com/github/chengpohi/connector/ElasticClientConnector.scala
+++ src/main/scala/com/github/chengpohi/connector/ElasticClientConnector.scala
@@ -1,10 +1,12 @@
 package com.github.chengpohi.connector
 
 import java.net.InetSocketAddress
+import java.nio.file.Paths
 
 import com.typesafe.config.ConfigFactory
 import org.elasticsearch.common.settings.Settings
 import org.elasticsearch.common.transport.InetSocketTransportAddress
+import org.elasticsearch.node.Node
 import org.elasticsearch.transport.client.PreBuiltTransportClient
 
 /**
@@ -14,15 +16,28 @@ import org.elasticsearch.transport.client.PreBuiltTransportClient
 object ElasticClientConnector {
   lazy val indexConfig = ConfigFactory.load("application.conf").getConfig("elastic")
   val clusterName: String = indexConfig.getString("cluster.name")
-  val settings = Settings.builder()
-    .put("cluster.name", clusterName)
-    .build()
-  val host: String = indexConfig.getString("host")
-  val port: Int = indexConfig.getInt("port")
+  val isLocal: Boolean = indexConfig.getBoolean("local")
 
-  val client = buildClient(settings, host, port)
+  val client = isLocal match {
+    case false => buildRemoteClient()
+    case true => buildLocalClient()
+  }
 
-  def buildClient(settings: Settings, host: String, port: Int) = {
+  def buildLocalClient() = {
+    val settings: Settings = Settings.builder()
+      .loadFromPath(Paths.get(getClass.getResource("/local.yml").toURI))
+      .build()
+    val node = new Node(settings).start()
+    node.client()
+  }
+
+  def buildRemoteClient() = {
+    val settings = Settings.builder()
+      .put("cluster.name", clusterName)
+      .build()
+    val host: String = indexConfig.getString("host")
+    val port: Int = indexConfig.getInt("port")
+
     val client = new PreBuiltTransportClient(settings)
     client.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(host, port)))
   }
