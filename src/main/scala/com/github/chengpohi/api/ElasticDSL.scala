5ff035b470e4dc03a3d834007fe453bce3855ab1
chengpohi <chengpohi@gmail.com>
2016-12-04 21:39:33 +0800

    DONE:aggs dsls
    Former-commit-id: 7b11c352f37d3be8fa01cb2c5f23c2baac413d50

--- src/main/scala/com/github/chengpohi/api/ElasticDSL.scala
+++ src/main/scala/com/github/chengpohi/api/ElasticDSL.scala
@@ -1,25 +1,26 @@
 package com.github.chengpohi.api
 
-import com.github.chengpohi.api.dsl.DSLDefinition
+import com.github.chengpohi.api.dsl._
 import org.elasticsearch.client.{Client, ClusterAdminClient}
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
 /**
- * elasticshell
- * Created by chengpohi on 1/6/16.
- */
-class ElasticDSL(cl: Client) extends ElasticManagement
-                      with ElasticIndexer
-                      with ElasticDocUpdater
-                      with ElasticDocDeleter
-                      with ElasticDocQuerier
-                      with ElasticAnalyzer
-                      with ElasticAggs
-                      with ElasticBase with DSLDefinition{
+  * elasticshell
+  * Created by chengpohi on 1/6/16.
+  */
+trait DSLs extends AggsDSL
+  with AnalyzeDSL
+  with DeleterDSL
+  with IndexerDSL
+  with ManageDSL
+  with QueryDSL
+
+class ElasticDSL(cl: Client) extends DSLs with DSLDefinition {
   val client: Client = cl
   val clusterClient: ClusterAdminClient = client.admin.cluster()
   val indicesClient = client.admin.indices()
+
   implicit def waitFuture[T](r: Future[T]): T = Await.result(r, Duration.Inf)
 }
