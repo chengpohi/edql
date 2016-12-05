75f7b7d63535032722e737757fa236ebe69faf90
Xiang Chen <chengpohi@gmail.com>
2016-12-05 22:28:27 +0800

    DONE:reback extract json
    Former-commit-id: 2ae460b5fc1dba00c0642106fe9917d2fafbf001

--- src/main/scala/com/github/chengpohi/api/dsl/DSLContext.scala
+++ src/main/scala/com/github/chengpohi/api/dsl/DSLContext.scala
@@ -35,6 +35,9 @@ import org.elasticsearch.search.SearchHit
 
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
+import org.json4s._
+import org.json4s.native.JsonMethods._
+import org.json4s.native.Serialization.write
 
 /**
   * Created by xiachen on 10/11/2016.
@@ -244,5 +247,26 @@ trait DSLContext {
 trait Definition[A] {
   def execute: Future[A]
 
+  def extract(path: String): ExtractDefinition = {
+    ExtractDefinition(this, path)
+  }
+
   def json: String
+}
+
+case class ExtractDefinition(definition: Definition[_], path: String) extends Definition[String] {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  implicit val formats = DefaultFormats
+
+  override def execute: Future[String] = {
+    Future {
+      val jObj = parse(definition.json)
+      val result = path.split("\\.").foldLeft(jObj) { (o, i) => o \ i }
+      write(result)
+    }
+  }
+
+  override def json: String = Await.result(execute, Duration.Inf)
 }
