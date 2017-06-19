2fd5f0f9d1c117a658b0cdaf9c1d51017e3278b8
chengpohi <chengpohi@gmail.com>
2017-06-19 21:42:53 +0800

    DONE:elasticdsl conf

--- src/main/scala/com/github/chengpohi/registry/ELKDSLContext.scala
+++ src/main/scala/com/github/chengpohi/registry/ELKDSLContext.scala
@@ -1,7 +1,7 @@
 package com.github.chengpohi.registry
 
 import com.github.chengpohi.api.ElasticDSL
-import com.github.chengpohi.connector.ElasticClientConnector
+import com.github.chengpohi.connector.ELKDSLConfig
 import com.github.chengpohi.helper.ResponseGenerator
 import com.github.chengpohi.parser.{ELKParser, InterceptFunction}
 
@@ -9,11 +9,11 @@ import com.github.chengpohi.parser.{ELKParser, InterceptFunction}
   * elasticdsl
   * Created by chengpohi on 4/4/16.
   */
-object ELKDSLContext {
+trait ELKDSLContext { this: ELKDSLConfig =>
+  private val client = buildClient(config)
+  private val interceptFunction = new InterceptFunction(dsl)
+
   implicit lazy val dsl: ElasticDSL = new ElasticDSL(client)
   implicit lazy val elkParser = new ELKParser(interceptFunction)
-
-  val responseGenerator = new ResponseGenerator
-  private val client = ElasticClientConnector.client
-  private val interceptFunction = new InterceptFunction(dsl)
+  implicit lazy val responseGenerator = new ResponseGenerator
 }
