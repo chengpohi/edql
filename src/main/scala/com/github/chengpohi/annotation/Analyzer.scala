8d433250d954f562e7ca1f79b46c2063a566570f
chengpohi <chengpohi@gmail.com>
2017-10-05 21:47:39 +0800

    [XiangChen]:index, alias, copyto annotations

--- src/main/scala/com/github/chengpohi/annotation/Analyzer.scala
+++ src/main/scala/com/github/chengpohi/annotation/Analyzer.scala
@@ -5,3 +5,12 @@ import scala.annotation.meta.field
 
 @field
 final class Analyzer(name: String = "standard") extends StaticAnnotation
+
+@field
+final class CopyTo(name: String) extends StaticAnnotation
+
+@field
+final class Index(name: String) extends StaticAnnotation
+
+@field
+final class Alias(name: String) extends StaticAnnotation
