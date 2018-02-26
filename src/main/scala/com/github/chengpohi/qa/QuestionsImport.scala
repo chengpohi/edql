197bd7d695aa457aae35b241e15fa1fd4bbd6acf
chengpohi <chengpohi@gmail.com>
2018-02-26 23:56:14 +0800

    upgrade to 6.x

---
+++ src/main/scala/com/github/chengpohi/qa/QuestionsImport.scala
@@ -0,0 +1,13 @@
+package com.github.chengpohi.qa
+
+import com.github.chengpohi.connector.ELKDSLConfig
+import com.github.chengpohi.registry.ELKDSLContext
+
+object QuestionsImport extends ELKDSLConfig with ELKDSLContext {
+
+  import dsl._
+
+  DSL {
+    index into "qa" / "question" doc Map("question" -> "question", "answers" -> List("answer1", "answer2"))
+  }.await
+}
