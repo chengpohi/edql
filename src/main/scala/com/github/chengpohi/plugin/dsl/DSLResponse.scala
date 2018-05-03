a44ecddb541013e459379e1693c3e8c756242a58
chengpohi <chengpohi@gmail.com>
2018-05-03 22:28:58 +0800

    TransportDSLAction

--- src/main/scala/com/github/chengpohi/plugin/dsl/DSLResponse.scala
+++ src/main/scala/com/github/chengpohi/plugin/dsl/DSLResponse.scala
@@ -3,8 +3,9 @@ package com.github.chengpohi.plugin.dsl
 import org.elasticsearch.action.ActionResponse
 import org.elasticsearch.common.xcontent.{ToXContent, ToXContentFragment, XContentBuilder}
 
-class DSLResponse extends ActionResponse with ToXContentFragment {
-  override def toXContent(builder: XContentBuilder, params: ToXContent.Params): XContentBuilder = {
-    builder
+case class DSLResponse(result: String) extends ActionResponse with ToXContentFragment {
+  override def toXContent(builder: XContentBuilder,
+                          params: ToXContent.Params): XContentBuilder = {
+    builder.field("result", result)
   }
 }
