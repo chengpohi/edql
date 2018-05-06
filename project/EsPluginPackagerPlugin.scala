60e2991ba69de2bb2593f278db4e2a61d0ec0400
chengpohi <chengpohi@gmail.com>
2018-05-07 00:18:37 +0800

    [TECH] - package to jar

--- project/EsPluginPackagerPlugin.scala
+++ project/EsPluginPackagerPlugin.scala
@@ -1,4 +1,8 @@
+import EsPluginPackager.packageToEsPlugin
+import sbt.Keys._
 import sbt._
+import sbtassembly.{Assembly, AssemblyOption, MergeStrategy, PathList}
+
 
 object EsPluginPackagerPlugin extends AutoPlugin {
   override def requires: AutoPlugin = plugins.JvmPlugin
@@ -14,7 +18,30 @@ object EsPluginPackagerPlugin extends AutoPlugin {
   import autoImport.{ESPluginPackager => _, baseEsPluginSettings => _, _}
 
   lazy val baseEsPluginPackagerSettings: Seq[sbt.Def.Setting[_]] = Seq(
-    esplugin := EsPluginPackager.assemblyTask(esplugin).value
+    logLevel in espackage := Level.Info,
+    assemblyOption in espackage := {
+      val s = streams.value
+      AssemblyOption(
+        assemblyDirectory = s.cacheDirectory / "espackage",
+        includeBin = true,
+        includeScala = true,
+        includeDependency = true,
+        mergeStrategy = {
+          case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+          case _ => MergeStrategy.first
+        },
+        excludedJars = Seq(),
+        excludedFiles = Assembly.defaultExcludedFiles,
+        cacheOutput = true,
+        cacheUnzip = true,
+        appendContentHash = false,
+        prependShellScript = None,
+        maxHashLength = None,
+        shadeRules = Seq(),
+        level = (logLevel in espackage).value
+      )
+    },
+    espackage := packageToEsPlugin(espackage).value
   )
 
   lazy val esPluginSettings: Seq[sbt.Def.Setting[_]] = baseEsPluginPackagerSettings
