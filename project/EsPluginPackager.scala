3248c0e007ec4e9cfcb98b5f3ba24c2416c440ce
chengpohi <chengpohi@gmail.com>
2018-05-07 00:18:37 +0800

    [TECH] - package to jar

--- project/EsPluginPackager.scala
+++ project/EsPluginPackager.scala
@@ -1,12 +1,26 @@
 import sbt.Def.Initialize
+import sbt.Keys._
 import sbt._
+import sbtassembly.Assembly
 
 object EsPluginPackager {
 
   import EsPluginPackagerPlugin.autoImport.{ESPluginPackager => _, _}
 
-  def assemblyTask(key: TaskKey[Unit]): Initialize[Task[Unit]] = Def.task {
+  def packageToEsPlugin(key: TaskKey[File]): Initialize[Task[File]] = Def.task {
+    val s = (streams in key).value
     val pn = (pluginName in key).value
-    println(pn)
+    val pd = (pluginDescription in key).value
+    val pc = (pluginClassName in key).value
+
+    Assembly(
+      new File(s"${crossTarget.value}/$pn"),
+      (assemblyOption in key).value,
+      (packageOptions in(Compile, packageBin)).value,
+      Assembly.assembledMappingsTask(key).value,
+      s.cacheDirectory,
+      s.log
+    )
   }
+
 }
