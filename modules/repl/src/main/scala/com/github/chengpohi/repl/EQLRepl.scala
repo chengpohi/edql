706f426b65e9493e213b82f425f7dbde8625e5c8
chengpohi <chengpohi@gmail.com>
2021-02-20 23:04:47 +0800

    run script file

--- modules/repl/src/main/scala/com/github/chengpohi/repl/EQLRepl.scala
+++ modules/repl/src/main/scala/com/github/chengpohi/repl/EQLRepl.scala
@@ -5,29 +5,30 @@ import cats.data.Reader
 import cats.effect._
 import com.github.chengpohi.context.{EQLConfig, EQLContext}
 import com.github.chengpohi.dsl.serializer.JSONOps
+import com.github.chengpohi.script.EQLScriptRunner
 import jline.console.ConsoleReader
 import jline.console.history.FileHistory
 import jline.internal.Configuration
-import joptsimple.OptionParser
-import org.apache.commons.lang3.StringUtils
 import org.apache.lucene.util.IOUtils
 
 import java.io.File
-import scala.collection.JavaConverters._
 import scala.io.Source
 
-object EQLRepl extends EQLConfig with EQLContext with JSONOps {
+object EQLRepl extends EQLConfig
+  with EQLContext
+  with JSONOps {
   val ANSI_GREEN = "\u001B[32m"
   val ANSI_RESET = "\u001B[0m"
 
-  val elkInterpreter = new EQLInterpreter()
+  val eqlInterpreter = new EQLInterpreter()
+  val scriptRunner = new EQLScriptRunner(eqlInterpreter)
 
   def interpret(line: String): Reader[EQLInterpreter, String] = {
-    Reader((elkRunEngine: EQLInterpreter) => elkRunEngine.run(line).beautify)
+    Reader((interpreter: EQLInterpreter) => interpreter.run(line).beautify)
   }
 
   def run[T](reader: Reader[EQLInterpreter, T]): Id[T] = {
-    reader(elkInterpreter)
+    reader(eqlInterpreter)
   }
 
   def addShutdownHook(reader: ConsoleReader): Unit = {
@@ -74,7 +75,7 @@ object EQLRepl extends EQLConfig with EQLContext with JSONOps {
   }
 
 
-  def repl() = {
+  def runRepl() = {
     println(ANSI_GREEN + "Welcome to EQL Repl :)" + ANSI_RESET)
     val reader: ConsoleReader = buildReader
     while (true) {
@@ -93,22 +94,19 @@ object EQLRepl extends EQLConfig with EQLContext with JSONOps {
 
 
   def main(args: Array[String]): Unit = {
-    val parser = new OptionParser();
-    val scriptFileOpt = parser.acceptsAll(List("f", "file").asJava, "script file")
-      .withOptionalArg()
-    val optionSets = parser.parse(args: _*)
-    val scriptFile = optionSets.valueOf(scriptFileOpt)
-    if (StringUtils.isNotBlank(scriptFile)) {
-      val script = Resource
-        .fromAutoCloseable(IO {
-          Source.fromURL(scriptFile)
-        })
-        .use(i => IO(i.getLines().mkString(System.lineSeparator())))
-        .unsafeRunSync()
-      val result = run(interpret(script))
-      println(result)
+    try {
+      scriptRunner.run match {
+        case Some(res) => {
+          println(res)
+          System.exit(0)
+        }
+        case None => runRepl()
+      }
+    } catch {
+      case ex: Exception => {
+        ex.printStackTrace()
+        System.exit(-1)
+      }
     }
-
-    repl()
   }
 }
