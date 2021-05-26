623514b9e6bb22e868e2481c52e0191cfe9f992c
chenxiang25 <chenxiang25@meituan.com>
2021-05-26 16:24:56 +0800

    function invoke

--- modules/repl/src/main/scala/com/github/chengpohi/script/EQLScriptRunner.scala
+++ modules/repl/src/main/scala/com/github/chengpohi/script/EQLScriptRunner.scala
@@ -9,6 +9,7 @@ import com.typesafe.config.{Config, ConfigFactory}
 
 import java.io.File
 import java.net.URL
+import scala.collection.mutable
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
 
@@ -37,7 +38,7 @@ class EQLScriptRunner {
     eqlParser.parseJson(text)
   }
 
-  def extractVars(text: String): Map[String, JsonCollection.Val] = {
+  def extractVars(text: String): Map[String, Either[JsonCollection.Val, FunctionInvokeInstruction]] = {
     val instructions = this.generateInstructions(text)
     instructions.map(ins => {
       val cIns = ins.filter(_.isInstanceOf[ScriptContextInstruction2])
@@ -47,12 +48,18 @@ class EQLScriptRunner {
         .toMap
     }) match {
       case Success(value) => value
-      case Failure(_) => Map[String, JsonCollection.Val]()
+      case Failure(_) => Map()
     }
   }
 
-  def run(script: String): Try[Seq[Seq[String]]] = {
+  def run(script: String, targetInstruction: Option[String] = None): Try[Seq[Seq[String]]] = {
     val instructions = this.generateInstructions(script)
+    val selectedInstruction = targetInstruction.map(i => this.generateInstructions(i))
+
+    if (targetInstruction.isDefined && selectedInstruction.get.isFailure) {
+      return Failure(selectedInstruction.get.failed.get)
+    }
+
     instructions.map(ins => {
       val rIns = ins.filter(!_.isInstanceOf[ScriptContextInstruction2])
       val cIns = ins.filter(_.isInstanceOf[ScriptContextInstruction2])
@@ -72,13 +79,37 @@ class EQLScriptRunner {
             .map(i => i.asInstanceOf[FunctionInstruction])
             .map(i => i.funcName + i.variableNames.size -> i).toMap
 
-          val context = ScriptEQLContext(hostInstruction2.endpoint, aInstruction2, timeout, vars)
-          rIns.map {
-            case f: FunctionInvokeInstruction =>
-              functionInvoke(functions, context, f)
-            case i => {
-              Seq(i.execute(context).json)
+          val dVars = vars.filter(_._2.isLeft).map(i => i._1 -> i._2.left.get)
+          val fVars = vars.filter(_._2.isRight).map(i => i._1 -> i._2.right.get)
+
+          val context = ScriptEQLContext(hostInstruction2.endpoint, aInstruction2, timeout, dVars)
+          fVars.foreach(fVar => {
+            val value = functionInvoke(functions, context, fVar._2).last
+            val fVal = parseJson(value)
+            if (fVal.isFailure) {
+              return Failure(fVal.failed.get)
             }
+            context.variables.put(fVar._1, fVal.get)
+          })
+
+          selectedInstruction match {
+            case Some(ss) =>
+              val ins = ss.get
+              ins.map {
+                case f: FunctionInvokeInstruction =>
+                  functionInvoke(functions, context, f)
+                case i => {
+                  Seq(i.execute(context).json)
+                }
+              }
+            case None =>
+              rIns.map {
+                case f: FunctionInvokeInstruction =>
+                  functionInvoke(functions, context, f)
+                case i => {
+                  Seq(i.execute(context).json)
+                }
+              }
           }
         case None =>
           return Failure(new RuntimeException("Please set host bind"))
@@ -102,21 +133,35 @@ class EQLScriptRunner {
       .map(i => i.asInstanceOf[VariableInstruction])
       .map(i => i.variableName -> i.value).toMap
 
-    context.variables = (variables.toSeq ++ vars.toSeq ++ funcVars).groupBy(_._1).view.mapValues(_.map(_._2).head).toMap
-    val response = func.instructions.filter(!_.isInstanceOf[ScriptContextInstruction2]).flatMap {
+    val dVars = funcVars.filter(_._2.isLeft).map(i => i._1 -> i._2.left.get)
+    val seqVars = (vars.toSeq ++ variables.toSeq ++ dVars).groupBy(_._1).view.mapValues(_.map(_._2).head).toSeq
+    context.variables = mutable.Map(seqVars: _*)
+    val fVars = funcVars.filter(_._2.isRight).map(i => i._1 -> i._2.right.get)
+    fVars.foreach(fVar => {
+      val value = functionInvoke(functions, context, fVar._2).last
+      val fVal = parseJson(value)
+      if (fVal.isFailure) {
+        throw new RuntimeException(fVal.failed.get)
+      }
+      context.variables.put(fVar._1, fVal.get)
+    })
+
+    val response = func.instructions
+      .filter(!_.isInstanceOf[ScriptContextInstruction2]).flatMap {
       case f: FunctionInvokeInstruction =>
         functionInvoke(functions, context, f)
+      case r: ReturnInstruction => {
+        r.value match {
+          case Left(jv) => Seq(jv.toJson)
+          case Right(f) => functionInvoke(functions, context, f)
+        }
+      }
       case i => {
         Seq(i.execute(context).json)
       }
     }
-
     context.variables = variables
     response
-  }
-
-  private def scriptContextInstruction(i: eqlParser.Instruction2) = {
-    i.isInstanceOf[ScriptContextInstruction2]
   }
 
   def getScriptFilePathFromEnv: Option[String] = {
@@ -137,7 +182,7 @@ object ScriptEQLContext {
   def apply(endpoint: String, auth: Option[String] = None, timeout: Option[Int], vars: Map[String, JsonCollection.Val]): ScriptEQLContext = {
     val url = new URL(endpoint)
     val context = new ScriptEQLContext(url.getHost, url.getPort, auth, timeout)
-    context.variables = vars
+    context.variables = mutable.Map[String, JsonCollection.Val](vars.toSeq: _*)
     context
   }
 }
