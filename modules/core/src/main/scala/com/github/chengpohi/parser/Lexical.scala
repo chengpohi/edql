640335361d519de4d5527c981edef2efb7520dec
chengpohi <chengpohi@gmail.com>
2022-08-10 14:26:21 +0800

    longstring

--- modules/core/src/main/scala/com/github/chengpohi/parser/Lexical.scala
+++ modules/core/src/main/scala/com/github/chengpohi/parser/Lexical.scala
@@ -31,7 +31,7 @@ object Lexical {
 
   def longstring[_: P](delimiter: String) = P(WS ~ delimiter ~ longstringitem(delimiter).rep.! ~ delimiter ~ WS)
     .map(i => {
-      JsonCollection.Str(StringEscapeUtils.escapeJava(i).replaceAll("\\r|\\n", ""))
+      JsonCollection.Str(StringEscapeUtils.escapeJava(i).replaceAll("\\r|\\n|\\\\n|\\\\r", ""))
     })
 
   def longstringitem[_: P](quote: String): P[Unit] = P(longstringchar(quote) | escapeseq | !quote ~ quote.take(1))
