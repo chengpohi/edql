902d88fa7348b961b18fd07595637fe146747e03
chengpohi <chengpohi@gmail.com>
2022-10-06 10:46:04 +0800

    enable empty body

--- modules/repl/src/test/scala/com/github/chengpohi/PerformanceTest.scala
+++
@@ -1,9 +0,0 @@
-package com.github.chengpohi
-
-import com.github.chengpohi.helper.EQLTestTrait
-
-/**
-  * Created by xiachen on 31/12/2016.
-  */
-class PerformanceTest extends EQLTestTrait {
-}
--- modules/repl/src/test/scala/com/github/chengpohi/dsl/eql/usage/TermsVectorUsage.scala
+++
@@ -1,6 +0,0 @@
-package com.github.chengpohi.dsl.eql.usage
-
-import com.github.chengpohi.helper.EQLTestTrait
-
-object TermsVectorUsage extends EQLTestTrait {
-}
