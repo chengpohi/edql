5787ad8cc488954329644ca2c25cfe3aacf00ade
chengpohi <chengpohi@gmail.com>
2023-05-29 17:13:49 +0800

    should not remove scope

--- modules/core/src/main/scala/com/github/chengpohi/parser/InterceptFunction.scala
+++ modules/core/src/main/scala/com/github/chengpohi/parser/InterceptFunction.scala
@@ -378,7 +378,7 @@ trait InterceptFunction {
     variables.filter(i => Option.apply(invokePath).forall(j => i._1.startsWith(j)))
       .filter(_._2 != null)
       .foldLeft(path)((i, o) => {
-        val vName = o._1.replaceAll(invokePath, "");
+        val vName = o._1
         val v: String = o._2 match {
           case s: JsonCollection.Str => {
             s.value
